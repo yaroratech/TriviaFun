664df8eb7ddfd2ac95042b60350ba00d1d8ca9d8
yaroratech <yarora31@gatech.edu>
2021-09-06 17:47:43 -0400

    commiting 4 files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Input, Button } from "@material-ui/core";
+import Question from "./Question";
 
-function App() {
+export default function App() {
+  const [questions, setQuestions] = useState([]);
+  const [numQuestions, setNumQuestions] = useState(10);
+  const [numAnswered, setNumAnswered] = useState(0);
+
+  console.log(numQuestions);
+
+  const handleChange = (e) => {
+    setNumQuestions(e.target.value);
+  };
+
+  const fetchQuestions = () => {
+    fetch("https://opentdb.com/api.php?amount=" + numQuestions)
+      .then((res) => res.json())
+      .then((res) => {
+        const questions = res.results.map((question) => {
+          let answers;
+
+          if (question.type === "boolean") {
+            answers = ["True", "False"];
+          } else {
+            answers = [...question.incorrect_answers];
+            const rand = Math.floor(Math.random() * 3);
+
+            answers.splice(rand, 0, question.correct_answer);
+          }
+
+          return { ...question, answers };
+        });
+
+        setQuestions(questions);
+      });
+
+    setNumAnswered(0);
+  };
+
+  if (questions.length === 0) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Input type="number" value={numQuestions} onChange={handleChange} />
+        <Button onClick={fetchQuestions}>Go!</Button>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      {questions.map((q) => (
+        <Question
+          question={q}
+          setNumAnswered={setNumAnswered}
+          key={q.question}
+        />
+      ))}
+
+      {numAnswered === parseInt(numQuestions) && (
+        <Button onClick={fetchQuestions}>Fetch more questions</Button>
+      )}
     </div>
   );
 }
-
-export default App;
